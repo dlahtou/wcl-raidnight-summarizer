42862be95655bc927b17b9fafaabf98019da982c
Dan Ah Tou <dlahtou@gmail.com>
2019-01-03 15:59:43 -0800

    folder management and commenting

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from raid_night_summarizer import RaidnightData, complete_report, get_best_ilvl_avg_improvement
 import argparse
-from os.path import realpath, dirname, join
-from os import pardir
+from os.path import realpath, dirname, join, isdir
+from os import pardir, mkdir
 
 if __name__ == '__main__':
+    # Setup filepaths
     basedir = dirname(realpath(__file__))
-    print(basedir)
+    if not isdir(join(basedir, 'reports')):
+        mkdir(join(basedir, 'reports'))
+    raid_folder = join(basedir, args.dir if args.dir else 'MyDudes')
 
+    # Input arguments
     parser = argparse.ArgumentParser(description='Generate reports for warcraftlogs raid data')
     parser.add_argument('code', type=str, 
                         help="the 16-character string identifier for a report")
@@ -15,13 +19,15 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    raid_folder = join(basedir, args.dir if args.dir else 'MyDudes')
-
+    # Create list of raid codes to lookup on warcraftlogs
     raidlist = [args.code]
 
+    # Generate output file for each raid code
     for raid_id in raidlist:
         raidnight = RaidnightData(raid_id, raid_folder)
+        out_path = join(basedir, f'reports/{raidnight.name}.txt')
 
-        complete_report(raidnight, raid_folder, join(basedir, f'reports/{raidnight.name}.txt'), raid_id, improved=True)
+        complete_report(raidnight, raid_folder, out_path, raid_id, improved=True)
 
+        # optional logging of most-improved raiders
         print(get_best_ilvl_avg_improvement(raidnight, raid_folder, 5))
